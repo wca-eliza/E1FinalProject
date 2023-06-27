dcee8acba683fdfbc4de9e347f19ec2031c899df
Eliza Wang <elizawang0728@gmail.com>
2023-06-27 09:56:44 -0400

    Initial Commit

---
+++ E1FinalProject/E1FinalProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  E1FinalProjectApp.swift
+//  E1FinalProject
+//
+//  Created by Eliza Wang on 2023-06-27.
+//
+
+import SwiftUI
+
+@main
+struct E1FinalProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
